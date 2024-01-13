b7a7839f8acd5e8e3036dd531e72bc4ffc92fe7e
Bagoole ivan <ivanbagoole2001@gmail.com>
2024-01-13 23:19:41 +0300

    home page

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const date = newDate();
+console.log(date.toLocaleDateString("en-US"));
+
+alert(document.lastModified);
+let oLastModified = newDate(document.lastModified);
+console.log(oLastModified);
